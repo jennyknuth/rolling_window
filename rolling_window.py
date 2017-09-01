18590e7d55e074634bf70c300ee6248b995d31aa
Jenny Knuth <jknuth@n.io>
2017-08-31 18:26:14 -0600

    working tests

--- rolling_window.py
+++ rolling_window.py
@@ -1,12 +1,27 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty
-
+from nio.properties import VersionProperty, IntProperty, StringProperty
 
 class RollingWindow(Block):
 
     version = VersionProperty('0.1.0')
+    chunk_size = IntProperty(title='Chunk Size', default=1)
+    step_by = IntProperty(title='Step By', default=1)
+    key = StringProperty(title='Key', default='')
 
     def process_signals(self, signals):
+        out_sigs = []
         for signal in signals:
-            pass
-        self.notify_signals(signals)
+            key = self.key(signal)
+            if key in signal:
+                data = signal[key]
+                for i in range(len(data) - (self.chunk_size(signal) * self.step_by(signal)) + 1):
+                    chunk = []
+                    for x in range(self.chunk_size(signal)):
+                        # if ((i + self.chunk_size(signal)) * self.step_by(signal) <= len(data)):
+                        chunk.append(data[(i * self.step_by(signal)) + x])
+                        print(chunk)
+
+                    # if len(chunk):
+                    out_sigs.append(chunk)
+        print(out_sigs)
+        self.notify_signals(out_sigs)
--- tests/rolling_window.py
+++ tests/rolling_window.py
@@ -1,19 +1,69 @@
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..rolling_window import RollingWindow
 
+# foo=[1, 2, 3, 4, 5, 6]
+# chunk_size=3
+# bar=[[foo[i+x] for x in range(chunk_size)] for i in range(len(foo)-chunk_size)]
+# bar
+# [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+# [[1, 2, 3], [3,4,5]]
+# [sum(b)/len(b) for b in bar]
+# [2.0, 3.0, 4.0]
+# for x in [sum(b)/len(b) for b in bar]:
+# ...     print(x)
+# ...
+# 2.0
+# 3.0
+# 4.0
 
 class TestRollingWindow(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
-        blk = Example()
-        self.configure_block(blk, {})
+    def test_int_array(self):
+        blk = RollingWindow()
+        sig = [{'data': [1, 2, 3, 4, 5, 6]}]
+        self.configure_block(blk, {
+            'chunk_size': 3,
+            'step_by': 1,
+            'key': 'data'
+        })
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.process_signals(sig)
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+        self.assert_num_signals_notified(4)
+        self.assertEqual(
+            [1, 2, 3], self.last_notified[DEFAULT_TERMINAL][0])
+        self.assertEqual(
+            [4, 5, 6], self.last_notified[DEFAULT_TERMINAL][-1])
+    def test_no_key(self):
+        blk = RollingWindow()
+        sig = [{'data': [1, 2, 3, 4, 5, 6]}]
+        self.configure_block(blk, {
+            'chunk_size': 3,
+            'step_by': 1,
+            'key': ''
+        })
+        blk.start()
+        blk.process_signals(sig)
+        blk.stop()
+        self.assert_num_signals_notified(0)
+        #todo raise exception
+        # self.assertEqual(
+        #     [1, 2, 3], self.last_notified[DEFAULT_TERMINAL][0])
+
+    def test_step_by_2(self):
+        """step by 2"""
+        blk = RollingWindow()
+        sig = [{'data': [1, 2, 3, 4, 5, 6, 7]}]
+        self.configure_block(blk, {
+            'chunk_size': 3,
+            'step_by': 2,
+            'key': 'data'
+        })
+        blk.start()
+        blk.process_signals(sig)
+        blk.stop()
+        self.assert_num_signals_notified(2)
+        self.assertEqual(
+            [3, 4, 5], self.last_notified[DEFAULT_TERMINAL][-1])
